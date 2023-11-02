a6bd07094faf7a840995aa8ce384365dcdb9c31a
Yordan <yordanpz@hotmail.com>
2023-11-02 17:23:19 -0300

    new database

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+
+  host: PGHOST,
+  
+  dialect: 'postgres',
+  
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+      project: ENDPOINT_ID
+    }
+  }
+
+});
 
 module.exports = sequelize;
